0e83b8873667dc6c0bcaf4070d9cb549b727f605
Nainakumari <iit2019004@iiita.ac.in>
2020-12-19 13:09:17 +0530

    added

---
+++ amazona/backend/data.js
@@ -0,0 +1,34 @@
+export default {
+    products:   [
+    {
+        id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        id:'2',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        id:'3',
+        name:'Slim Shirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    ]
+}
